deaa12bd4f8d5ef95f662ae97551451474b22605
Navjot Kaur <navjotkaur8838@gmail.com>
2025-04-04 11:35:46 +0530

    updated

--- frontend/data.js
+++ frontend/data.js
@@ -1,19 +1,19 @@
 export const highlightedWork = {
-        image:"helping-patients.jpg",
+        image:"/images/helping-patients.jpg",
         title:"Helping Blind & Leprosy Patients",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         button: "Contribute",
 }
 
 export const works = [
-    { image: "blood-donation.jpg", title: "Blood donation & medical camps" },
-    { image: "food-distribution.jpg", title: "Food Distribution" },
-    { image: "old-age-home.jpg", title: "Old age home" },
-    { image: "underprivileged-help.jpg", title: "Helping underprivilege" }
+    { image: "/images/blood-donation.jpg", title: "Blood donation & medical camps" },
+    { image: "/images/food-distribution.jpg", title: "Food Distribution" },
+    { image: "/images/old-age-home.jpg", title: "Old age home" },
+    { image: "/images/underprivileged-help.jpg", title: "Helping underprivilege" }
   ];
 
 export const storyData = [
-  { titledescription: 'hfghhbdtrhv'},
-  { image: "A journey of hope and support"},
-  { description: "fdfhsldioclodic"}
+  { titledescription: ''},
+  { image: "/images/A journey of hope and support"},
+  { description: ''}
 ]
