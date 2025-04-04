eb9535ac4cd2b79d6039a4b8e875eb4852639554
Navjot Kaur <navjotkaur8838@gmail.com>
2025-04-04 12:11:53 +0530

    updated

--- frontend/data.js
+++ frontend/data.js
@@ -1,15 +1,14 @@
 export const highlightedWork = {
-        image:"/images/helping-patients.jpg",
+        image:'/images/helping-patients.png',
         title:"Helping Blind & Leprosy Patients",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        button: "Contribute",
 }
 
 export const works = [
-    { image: "/images/blood-donation.jpg", title: "Blood donation & medical camps" },
-    { image: "/images/food-distribution.jpg", title: "Food Distribution" },
-    { image: "/images/old-age-home.jpg", title: "Old age home" },
-    { image: "/images/underprivileged-help.jpg", title: "Helping underprivilege" }
+    { image: "/images/blood-donation.png", title: "Blood donation & medical camps" },
+    { image: "/images/food-distribution.png", title: "Food Distribution" },
+    { image: "/images/old-age-home.png", title: "Old age home" },
+    { image: "/images/help-underprivileged.png", title: "Helping underprivilege" }
   ];
 
 export const storyData = [
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Card = ({ image, title, description, button, onClick}) => {
+const Card = ({ image, title, description, onClick}) => {
   return (
     <div className='rounded-lg p-2 flex items-center'>
-      <img src={image} alt={title} className="w-1/2 rounded-lg"/>
-      <div className="flex flex-col items-center bg-pink-100 w-1/2 rounded-xl h-full p-4 min-h-[200px]">
+      <img src={image} alt={title} className="w-1/2 h-64 rounded-lg"/>
+      <div className="flex flex-col items-center bg-pink-100 w-1/2 rounded-xl h-64 p-4 min-h-[200px]">
         <h3 className="text-lg font-semibold mt-2 mb-2 text-center">{title}</h3>
         <p className="text-gray-600 text-sm text-center mb-2">{description}</p>
         <button
           className="mt-6 bg-orange-400 text-white px-4 py-2 rounded-3xl hover:bg-orange-500"
           onClick={onClick}
         >
-          {button}
+          Contribution
         </button>
       </div>
     </div>
